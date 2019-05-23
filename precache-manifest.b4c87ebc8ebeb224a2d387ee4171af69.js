65c38ac34495ffdb6833bd77a09a449fd6e8d341
vaniaparamita <vania.parammita23@gmail.com>
2019-05-23 09:41:28 +0700

    Updates

---
+++ precache-manifest.b4c87ebc8ebeb224a2d387ee4171af69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2913b134abc027b0368bfa2061c57629",
+    "url": "/responsiweb/index.html"
+  },
+  {
+    "revision": "350c5e979d0b9b107909",
+    "url": "/responsiweb/static/css/main.8a740141.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiweb/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "350c5e979d0b9b107909",
+    "url": "/responsiweb/static/js/main.394d55fd.chunk.js"
+  },
+  {
+    "revision": "adae9c95a9a51b7aacd3",
+    "url": "/responsiweb/static/js/runtime~main.a04c41a8.js"
+  }
+]);
